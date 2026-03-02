7508e4cd82ef5c5cb76d59fcf4bf0ad74cd76400
yfsevim <yfsevim@gmail.com>
2026-03-02 23:29:38 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         "anthropic-version": "2023-06-01",
       },
       body: JSON.stringify({
-        model: "claude-3-5-sonnet-latest",
+       model: "claude-3-haiku-20240307",
         max_tokens: 800,
         messages: req.body.messages,
       }),
