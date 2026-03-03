c794907fe320444225636bb0a853dcad3ac848fe
yfsevim <yfsevim@gmail.com>
2026-03-03 21:30:11 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,11 @@
-module.exports = (req, res) => {
-  res.status(200).json({ message: "API çalışıyor" });
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  const { message } = req.body;
+
+  return res.status(200).json({
+    reply: "Mesaj alındı: " + message
+  });
 };
